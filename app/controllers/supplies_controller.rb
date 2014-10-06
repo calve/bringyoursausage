32b04af6d16229f97e4fbc5acea5974a59d2f79f
calve <calvinh34@gmail.com>
2014-10-07 00:10:36 +0200

    Really tempt to use

--- app/controllers/supplies_controller.rb
+++ app/controllers/supplies_controller.rb
@@ -1,6 +1,6 @@
 class SuppliesController < ApplicationController
   before_action :set_supply, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:edit, :new, :create]
+  before_action :authenticate_user!, only: [:edit, :create]
 
   # GET /supplies
   # GET /supplies.json
